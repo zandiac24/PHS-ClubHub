d52cc8f7a59c61a452f35cb7700df714b688a132
zandiac24 <brycefowlkes@gmail.com>
2025-06-03 21:46:38 -0400

    18.3
    Minor changes to approval page layout

--- app/dashboard/approval/page.tsx
+++ app/dashboard/approval/page.tsx
@@ -8,14 +8,14 @@ import ClearRosterButton from '@/app/ui/dashboard/clear-button';
 export default async function Page() {
    
     return (
-        <div>
-            <h1 className='text-[25px] font-semibold mt-[25px] mb-[10px] ml-[35px]'>Admin Panel</h1>
+        <div className="ml-[35px]">
+            <h1 className='text-[25px] font-semibold mt-[25px] mb-[10px]'>Admin Panel</h1>
             <div className='grid grid-cols-3 ml-[0px]'>
             <div>
-                <h1 className="mt-[30px] justify-center flex">Download Student Roster</h1>
+                <h1 className="mt-[30px]">Download Student Roster</h1>
                 <Link
                     href="https://console.neon.tech/app/projects/winter-scene-29137066/branches/br-little-band-a4exokvw/tables?database=neondb" 
-                className='flex items-center mx-auto mb-[30px] mt-[30px] justify-center rounded-md bg-yellow-100 w-[10vw] h-[6vh] text-md font-medium hover:bg-yellow-200'
+                className='flex items-center mb-[30px] mt-[30px] justify-center rounded-md bg-yellow-100 w-[10vw] h-[6vh] text-md font-medium hover:bg-yellow-200'
                 >
                 <p className="hidden md:block">Download Roster</p>
                 </Link>
@@ -26,9 +26,9 @@ export default async function Page() {
             </div>
 
             <div>
-            <h1 className="mt-[30px] justify-center flex">Update Club Information</h1>
+            <h1 className="mt-[30px]">Update Club Information</h1>
             <Link
-                className='flex items-center mb-[30px] mt-[30px] mx-auto justify-center rounded-md bg-yellow-100 w-[10vw] h-[6vh] text-md text-center font-medium hover:bg-yellow-200'
+                className='flex items-center mb-[30px] mt-[30px] ustify-center rounded-md bg-yellow-100 w-[10vw] h-[6vh] text-md text-center font-medium hover:bg-yellow-200'
                 href="/dashboard/update"
             >
                 Update Form
@@ -36,9 +36,9 @@ export default async function Page() {
             </div>
             </div>
             
-            <h2 className='text-[25px] ml-[35px] font-semibold mt-[50px] mb-[30px]'>Pending Clubs</h2>
+            <h2 className='text-[25px] font-semibold mt-[50px] mb-[30px]'>Pending Clubs</h2>
             
-            <div className='ml-[35px]'>
+            <div>
                     <PendingClubList />
             </div>
         </div>
