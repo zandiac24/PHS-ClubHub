804a10b2565df496e9af01504f0ddd71dae69822
zandiac24 <brycefowlkes@gmail.com>
2025-06-09 19:37:26 -0400

    Update 26
    Deleted test emails, made it so students are deleted if their club is deleted

--- app/api/reject/route.ts
+++ app/api/reject/route.ts
@@ -17,14 +17,19 @@ export async function POST(req: Request) {
     const {club_name} = body;
 
     //delete the club 
-    const query = `
+    const query1 = `
       DELETE FROM club_list
       WHERE club_name = $1;
+    `;
+     const query2 = `
+      DELETE FROM student_roster
+      WHERE club = $1;
     `;
 
     const values = [club_name];
 
-    const result = await pool.query(query, values);
+    const result = await pool.query(query1, values);
+    await pool.query(query2, values);
 
     return NextResponse.json({ success: true, data: result.rows[0] });
   } catch (error) {
